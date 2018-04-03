d48ead393711fc7696f0f6c049a63a5c2295767a
allan.pedroni <allan.pedroni@antt.gov.br>
2018-04-03 13:21:09 -0300

    Done 12. Removing an Object

--- courses/course-03/trunk/pepper/src/app/app.component.ts
+++ courses/course-03/trunk/pepper/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 // import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
@@ -8,21 +8,62 @@ import { Observable } from 'rxjs/Observable';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
-  cuisines = ['c1', 'c2', 'c2'];
+  // cuisines = ['c1', 'c2', 'c2'];
 
-  items: Observable<any[]>;
+  cuisines: Observable<any[]>;
+  cusineFireList: AngularFireList<any[]>;
+  restaurant: Observable<any[]>;
+  restaurantFireList: AngularFireObject<{}>;
 
   constructor(private db: AngularFireDatabase) {
-
-    this.items = this.db.list('/cuisines').valueChanges();
-
-    this.getCourses('/cuisines').subscribe(x => {
-      this.cuisines = x;
-    });
   }
-  getCourses(listPath): Observable<any[]> {
-    return this.db.list(listPath).valueChanges();
+
+  ngOnInit() {
+    this.cuisines = this.filterNodes('/cuisines');
+
+    this.restaurant = this.filterNode('/restaurant');
+    // this.restaurant.subscribe(x => {
+    //   console.log('restaurant', x);
+    // });
+
+    // this.cuisines.subscribe(x => {
+    //   console.log('cuisines', x);
+    // });
+  }
+
+  filterNodes(listPath): Observable<any[]> {
+
+    this.cusineFireList = this.db.list(listPath);
+
+    return this.cusineFireList.valueChanges();
+  }
+
+  filterNode(node): Observable<any[]> {
+
+    this.restaurantFireList = this.db.object(node);
+
+    return this.restaurantFireList.valueChanges();
+  }
+
+  add() {
+    this.cusineFireList.push(['asian']);
+  }
+
+  update() {
+    // update the node
+    this.restaurantFireList.update(
+      {address: {city: 'belo horizonte 11' }, 'cuisine': 'brazilian', 'name': 'new name', 'rating': 5 }
+    );
+
+    // replace the node values
+    this.restaurantFireList.set(
+      {address: {city: 'belo horizonte' }, 'cuisine': 'brazilian', 'name': 'new name', 'rating': 5 }
+    );
+  }
+
+  delete() {
+    this.restaurantFireList.remove();
   }
 }
