b6db56582ac18aa7541440c813b4f6a6b25d7692
allan.pedroni <allan.pedroni@antt.gov.br>
2018-04-26 12:54:36 -0300

    Done 123. The Setup

---
+++ courses/course-03/trunk/integration-testing-demo/src/app/1-voter/voter.component.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { VoterComponent } from './voter.component';
+
+describe('VoterComponent', () => {
+
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
+
+  beforeEach(() => {
+    // creating dynamic test module
+    TestBed.configureTestingModule({
+      declarations: [VoterComponent]
+    });
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
+    // fixture.nativeElement //root element
+    // fixture.debugElement //wrapper instance of component
+  });
+
+  it('should ', () => {
+  });
+});
