b7f3791fbc55f017eaff258384aa8b2314731e3f
allan.pedroni <allan.pedroni@antt.gov.br>
2018-06-13 13:49:52 -0300

    Done 50. Displaying Recipe Details

--- courses/course-04/trunk/first-project/src/app/app.module.ts
+++ courses/course-04/trunk/first-project/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeListDetailComponent } from './recipes/recipe-list-detail/recipe-list-detail.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
@@ -18,7 +18,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
-    RecipeListDetailComponent,
+    RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent
