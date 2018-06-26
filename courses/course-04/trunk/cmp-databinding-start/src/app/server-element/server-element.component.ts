07d80a8369bf5dbd3dd0a5d32637b8d8f3629c10
allan.pedroni <allan.pedroni@antt.gov.br>
2018-06-26 12:52:32 -0300

    Done 73. Seeing Lifecycle Hooks in Action

--- courses/course-04/trunk/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ courses/course-04/trunk/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +6,44 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.None // can be view in source code file
 })
-export class ServerElementComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string};
+export class ServerElementComponent implements
+  OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  @Input() element: { type: string, name: string, content: string };
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) { // called after a bound input property changes
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+
+  ngOnInit() { // called once the component is initialize
+    console.log('ngOnInit called');
+  }
+
+  ngDoCheck() { // called during every change detection run
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit() { // called after content (ng-content) has been project into view
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked() { // called every time the project content has been checked
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit() { // called after the component's view (and child views) has been initialized
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked() { // called every time the view (and child views) have been checked
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy(): void { // called once the component is about to be destroyed
+    console.log('ngOnDestroy called');
   }
 
 }
