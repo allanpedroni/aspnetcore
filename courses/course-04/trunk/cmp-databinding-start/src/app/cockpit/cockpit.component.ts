83e2b20db8f77dd47bfbb967f1d5b5f9d7dc6c38
allan.pedroni <allan.pedroni@antt.gov.br>
2018-06-26 12:40:02 -0300

    Done 72. Understanding the Component Lifecycle

--- courses/course-04/trunk/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ courses/course-04/trunk/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,5 +1,5 @@
 import { Properties } from '../properties';
-import { Component, OnInit, Output, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Output, ViewChild, ElementRef, OnChanges } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 
 @Component({
@@ -17,8 +17,29 @@ export class CockpitComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  // ngOnChanges(): void { // called after a bound input property changes
+  // }
+
+  ngOnInit() { // called once the component is initialize
   }
+
+  // ngDoCheck() { //called during every change detection run
+  // }
+
+  // ngAfterContentInit() { // called after content (ng-content) has been project into view
+  // }
+
+  // ngAfterContentChecked() { // called every time the project content has been checked
+  // }
+
+  // ngAfterViewInit() { // called after the component's view (and child views) has been initialized
+  // }
+
+  // ngAfterViewChecked() { // called every time the view (and child views) have been checked
+  // }
+
+  // ngOnDestroy(): void { // called once the component is about to be destroyed
+  // }
 
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
