2459d22ded21b331641604c93636a4111a0f33b6
allan.pedroni <allan.pedroni@antt.gov.br>
2018-07-23 12:53:20 -0300

    Done 111. Adding Ingredients to Recipes

--- courses/course-04/trunk/first-project/src/app/recipes/recipe.model.ts
+++ courses/course-04/trunk/first-project/src/app/recipes/recipe.model.ts
@@ -1,11 +1,15 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingredients: Ingredient[];
 
-    constructor(n: string, d: string, ip: string) {
+    constructor(n: string, d: string, ip: string, ing: Ingredient[]) {
         this.name = n;
         this.description = d;
         this.imagePath = ip;
+        this.ingredients = ing;
     }
 }
--- courses/course-04/trunk/first-project/src/app/recipes/recipe.service.ts
+++ courses/course-04/trunk/first-project/src/app/recipes/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
@@ -6,8 +7,18 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test Recipe 1', 'This is simply a teste 1', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A test Recipe 2', 'This is simply a teste 2', 'https://cdn.pixabay.com/photo/2017/11/06/07/21/clay-2922934_960_720.png')
+    new Recipe('A test Recipe 1', 'This is simply a teste 1', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+    [
+      new Ingredient('Meat', 1),
+      new Ingredient('French Fries', 20)
+    ]),
+    new Recipe('Cheese Burguer', 'This is simply a teste 2', 'https://cdn.pixabay.com/photo/2017/11/06/07/21/clay-2922934_960_720.png',
+    [
+      new Ingredient('Bread', 2),
+      new Ingredient('Meat', 2),
+      new Ingredient('Bacon', 10),
+      new Ingredient('Cheese', 5),
+    ]),
   ];
 
   getRecipes() {
