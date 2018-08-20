95b09a3f608a2e9fddd74ef8f30a5a15172af4cc
allan.pedroni <allan.pedroni@antt.gov.br>
2018-08-20 12:32:30 -0300

    Done 156. Programmatic Navigation to the Edit Page

--- courses/course-04/trunk/first-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ courses/course-04/trunk/first-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { RecipeService } from './../recipe.service';
 import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -13,7 +13,8 @@ export class RecipeDetailComponent implements OnInit {
   id: number;
 
   constructor(private recipeService: RecipeService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router ) { }
 
   ngOnInit() {
     this.route.params
@@ -26,5 +27,10 @@ export class RecipeDetailComponent implements OnInit {
 
   OnAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], { relativeTo: this.route});
+    // this.router.navigate(['../', this.id, 'edit'], { relativeTo: this.route});
   }
 }
