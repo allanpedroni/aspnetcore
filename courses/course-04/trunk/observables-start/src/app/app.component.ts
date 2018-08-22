a08cc10c56eb5595806108e3d166dfaa86406876
allan.pedroni <allan.pedroni@antt.gov.br>
2018-08-22 12:55:01 -0300

    Done 166. Using Subjects to Pass AND Listen to Data

--- courses/course-04/trunk/observables-start/src/app/app.component.ts
+++ courses/course-04/trunk/observables-start/src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { UsersService } from './users.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  user1Activated = false;
+  user2Activated = false;
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.usersService.userActivated.subscribe(
+      (id: number) => {
+        this.user1Activated = (id === 1);
+        this.user2Activated = !this.user1Activated;
+      }
+    );
+  }
 }
