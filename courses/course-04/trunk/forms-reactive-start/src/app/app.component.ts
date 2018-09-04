f84ea4fabfcc576b8ab1717ed34c4036f091699b
allan.pedroni <allan.pedroni@antt.gov.br>
2018-09-04 12:57:45 -0300

    Done 204. Reactive: Setting and Patching Values

--- courses/course-04/trunk/forms-reactive-start/src/app/app.component.ts
+++ courses/course-04/trunk/forms-reactive-start/src/app/app.component.ts
@@ -27,10 +27,24 @@ export class AppComponent implements OnInit {
     this.signupForm.statusChanges.subscribe(
       (status) => console.log(status)
     );
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Allan',
+        'email': 'allan.barros@gmail.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Testando',
+      }
+    });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
