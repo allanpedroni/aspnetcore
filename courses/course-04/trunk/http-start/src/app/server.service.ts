24af0353bea29b5a3e419f7e9811c14d1a31525b
allan.barros@gmail.com <allan.barros@gmail.com>
2018-09-12 21:07:29 -0300

    Done 241. Adjusting Request Headers

--- courses/course-04/trunk/http-start/src/app/server.service.ts
+++ courses/course-04/trunk/http-start/src/app/server.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 
 @Injectable()
 export class ServerService {
@@ -8,6 +8,11 @@ export class ServerService {
   constructor(private http: Http) { }
 
   storeServers(servers: any[]) {
-    return this.http.post(this.url, servers);
+    const headers = new Headers({
+        'Content-Type': 'application/json'
+      });
+    return this.http.post(this.url,
+      servers,
+      {headers: headers});
   }
 }
