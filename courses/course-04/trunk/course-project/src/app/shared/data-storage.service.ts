15754942db763cc2b0feca2db87ec57fe38e50fb
allan.pedroni <allan.pedroni@antt.gov.br>
2018-09-14 12:51:35 -0300

    Done Transforming Response Data to Prevent Errors Seção 19, aula 254

--- courses/course-04/trunk/course-project/src/app/shared/data-storage.service.ts
+++ courses/course-04/trunk/course-project/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Recipe } from './../recipes/recipe.model';
 import { RecipeService } from './../recipes/recipe.service';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class DataStorageService {
@@ -16,9 +17,20 @@ export class DataStorageService {
 
   getRecipes() {
     return this.http.get(this.url)
-      .subscribe(
+      .pipe(map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
+          for (const recipe of recipes) {
+            if (!recipe['ingredients']) {
+              console.log(recipe);
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      ))
+      .subscribe(
+        (recipes: Recipe[]) => {
           this.recipeService.setRecipes(recipes);
         }
       );
