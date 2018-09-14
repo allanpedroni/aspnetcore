8a9eb247fc2cd81a1360be72586d7980905eef69
allan.pedroni <allan.pedroni@antt.gov.br>
2018-09-14 12:40:34 -0300

    Done GETting Back the Recipes Seção 19, aula 253

--- courses/course-04/trunk/course-project/src/app/shared/data-storage.service.ts
+++ courses/course-04/trunk/course-project/src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
+import { Recipe } from './../recipes/recipe.model';
 import { RecipeService } from './../recipes/recipe.service';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 
 @Injectable()
 export class DataStorageService {
@@ -11,5 +12,15 @@ export class DataStorageService {
 
   storeRecipes() {
     return this.http.put(this.url, this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    return this.http.get(this.url)
+      .subscribe(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 }
