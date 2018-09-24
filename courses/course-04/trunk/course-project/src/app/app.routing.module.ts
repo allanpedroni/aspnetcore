229384aac6e696ddf197cc269bf864251d5f6797
allan.pedroni <allan.pedroni@antt.gov.br>
2018-09-24 13:09:36 -0300

    Done Adding Lazy Loading to the Recipes ModuleSeção 21, aula 284

--- courses/course-04/trunk/course-project/src/app/app.routing.module.ts
+++ courses/course-04/trunk/course-project/src/app/app.routing.module.ts
@@ -1,12 +1,14 @@
+import { AuthGuardService } from './auth/auth-guard.service';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'shopping-list', component: ShoppingListComponent},
+  { path: '', component: HomeComponent },
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule', canLoad: [AuthGuardService] },
+  { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
 @NgModule({
