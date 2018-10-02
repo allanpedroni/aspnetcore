84a2d1b376ca3283cbe83d4fe02c95eee15cbd78
allan.pedroni <allan.pedroni@antt.gov.br>
2018-10-02 12:56:02 -0300

    Done Getting Started with Reducers Seção 24, aula 314

---
+++ courses/course-04/trunk/course-project/src/app/shopping-list/shopping-list.reducers.ts
@@ -0,0 +1,24 @@
+import { Action } from '@ngrx/store';
+
+import { Ingredient } from '../shared/ingredient.model';
+
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+
+const initialState = {
+  ingredients: [
+    new Ingredient('Apples', 2),
+    new Ingredient('Tomatoes', 3),
+    new Ingredient('Carrots', 5)
+  ]
+};
+
+export function shoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [ ...state.ingredients, action]
+      };
+  }
+  return state;
+}
