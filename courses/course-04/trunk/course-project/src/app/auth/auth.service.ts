ed0c8b09f4ece1a86c011202a8740d67fabd5b7e
allan.pedroni <allan.pedroni@antt.gov.br>
2018-10-05 12:46:39 -0300

    Done Dispatch Actions Seção 24, aula 331

--- courses/course-04/trunk/course-project/src/app/auth/auth.service.ts
+++ courses/course-04/trunk/course-project/src/app/auth/auth.service.ts
@@ -1,19 +1,25 @@
+import { Store } from '@ngrx/store';
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 // import * as firabase from 'firebase';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 @Injectable()
 export class AuthService {
-  token: string;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private store: Store<fromApp.AppState>) { }
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .then(
-        (value: firebase.auth.UserCredential) => console.log('then signupUser', value)
+        (value: firebase.auth.UserCredential) => {
+          console.log('then signupUser', value);
+          this.store.dispatch(new AuthActions.Signup());
+        }
       )
       .catch(
         error => console.log('erro signupUser', error)
@@ -26,25 +32,19 @@ export class AuthService {
         firebase.auth().currentUser.getIdToken()
           .then(
             (token: string) => {
-              this.token = token;
+              // this.token = token;
+              this.store.dispatch(new AuthActions.SetToken(token));
             }
           );
-          console.log('signinUser', response);
-          this.router.navigate(['/']);
+        console.log('signinUser', response);
+        this.store.dispatch(new AuthActions.Signin());
+        this.router.navigate(['/']);
       })
       .catch(error => console.log('erro signinUser', error));
   }
 
   logout() {
     firebase.auth().signOut();
-    this.token = null;
-  }
-
-  getToken() {
-    return this.token;
-  }
-
-  isAuthenticated() {
-    return this.token != null;
+    this.store.dispatch(new AuthActions.Logout());
   }
 }
