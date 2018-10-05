5c91824317db6f4dbfc76d78270b7d9ed8f2d4a5
allan.pedroni <allan.pedroni@antt.gov.br>
2018-10-05 12:28:25 -0300

    Done Adding Reducer Logic & Actions Seção 24, aula 328

--- courses/course-04/trunk/course-project/src/app/auth/store/auth.actions.ts
+++ courses/course-04/trunk/course-project/src/app/auth/store/auth.actions.ts
@@ -5,7 +5,6 @@ export const SIGNIN = 'SIGNIN';
 export const LOGOUT = 'LOGOUT';
 export const SET_TOKEN = 'SET_TOKEN';
 
-
 export class Signup implements Action {
   readonly type = SIGNUP;
 }
@@ -24,8 +23,4 @@ export class SetToken implements Action {
   constructor(public payload: string) {}
 }
 
-export type AuthActions =
-  Signup |
-  Signin |
-  Logout |
-  SetToken;
+export type AuthActions = Signup | Signin | Logout | SetToken;
