bb85437b81819a37f097ccc41dec17606c966245
allan.pedroni <allan.pedroni@antt.gov.br>
2018-11-13 13:03:41 -0200

    Done Only React to Actions Once via take(1) Seção 24, aula 335

--- courses/course-04/trunk/course-project/src/app/shared/auth.interceptor.ts
+++ courses/course-04/trunk/course-project/src/app/shared/auth.interceptor.ts
@@ -2,7 +2,7 @@ import { Store } from '@ngrx/store';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, take } from 'rxjs/operators';
 
 import * as fromApp from '../store/app.reducer';
 import * as fromAuth from '../auth/store/auth.reducer';
@@ -15,15 +15,12 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Interceptor working fine!', req);
     return this.store.select('auth')
+      .pipe(take(1))
       .pipe(
         switchMap(
           (authState: fromAuth.State) => {
             console.log('authState.token', authState.token);
-            const copiedReq = req.clone(
-              {
-                params: req.params.set('auth', authState.token)
-              }
-            );
+            const copiedReq = req.clone({params: req.params.set('auth', authState.token)});
             return next.handle(copiedReq);
           }));
   }
